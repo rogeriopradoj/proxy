19e8633839dc59d9c07f93aa0de9d5382edc9916
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2017-09-19 10:57:25 -0300

    Define NO_PROXY no arquivo .env

--- _config_init.bat
+++ _config_init.bat
@@ -1,5 +1,18 @@
 @echo off
 
+setlocal EnableDelayedExpansion
+
+set ENVFILE="%~dp0.env"
+IF NOT EXIST %ENVFILE% (
+    echo "Arquivo %ENVFILE% nao localizado. Dica: copie o arquivo .env.example da raiz do projeto"
+    goto:eof
+)
+
+call:GetEnvValue "CNTLM_NO_PROXY" CNTLM_NO_PROXY
+call:GetEnvValue "CNTLM_LISTEN" CNTLM_LISTEN
+call:GetEnvValue "CNTLM_PROXY" CNTLM_PROXY
+call:GetEnvValue "LOCAL_PROXY_ADDRESS" LOCAL_PROXY_ADDRESS
+
 echo.
 echo "==> set PROXY_WINDOWS_PATH environment variable"
 SET PROXY_WINDOWS_PATH=%~dp0 >nul 2>&1
@@ -11,13 +24,22 @@ echo ".Ok"
 
 echo.
 echo "==> set HTTP_PROXY and HTTPS_PROXY environment variables"
-SET HTTP_PROXY=http://127.0.0.1:8118 >nul 2>&1
+SET HTTP_PROXY=!LOCAL_PROXY_ADDRESS! >nul 2>&1
 SET HTTPS_PROXY=%HTTP_PROXY%  >nul 2>&1
 setx HTTP_PROXY %HTTP_PROXY% >nul 2>&1
 setx HTTPS_PROXY %HTTP_PROXY% >nul 2>&1
 echo.
 echo "   - HTTP_PROXY : " %HTTP_PROXY%
 echo "   - HTTPS_PROXY: " %HTTPS_PROXY%
+echo.
+echo ".Ok"
+
+echo.
+echo "==> set NO_PROXY environment variable"
+SET NO_PROXY=!CNTLM_NO_PROXY! >nul 2>&1
+setx NO_PROXY %NO_PROXY% >nul 2>&1
+echo.
+echo "   - NO_PROXY : " %NO_PROXY%
 echo.
 echo ".Ok"
 
@@ -35,3 +57,8 @@ echo ".Ok"
 
 echo.
 PAUSE
+
+
+:GetEnvValue
+@FOR /F "tokens=2 delims==" %%a in ('find "%~1=" %ENVFILE%') do @set %~2=%%a
+@goto:eof
--- _config_remove.bat
+++ _config_remove.bat
@@ -3,13 +3,23 @@
 echo.
 echo "==> unset PROXY_WINDOWS_PATH environment variable"
 REG delete HKCU\Environment /F /V PROXY_WINDOWS_PATH >nul 2>&1
+SET PROXY_WINDOWS_PATH=
 echo.
 echo ".Ok"
 
 echo.
 echo "==> unset HTTP_PROXY and HTTPS_PROXY environment variables"
 REG delete HKCU\Environment /F /V HTTP_PROXY >nul 2>&1
+SET HTTP_PROXY=
 REG delete HKCU\Environment /F /V HTTPS_PROXY >nul 2>&1
+SET HTTPS_PROXY=
+echo.
+echo ".Ok"
+
+echo.
+echo "==> unset NO_PROXY environment variable"
+REG delete HKCU\Environment /F /V NO_PROXY >nul 2>&1
+SET NO_PROXY=
 echo.
 echo ".Ok"
 
