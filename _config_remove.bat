c0ae6e355a18ac3ddfa4661812b22193701b0710
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2021-05-21 21:45:03 -0300

    Ajusta _config_remove.bat

--- _config_remove.bat
+++ _config_remove.bat
@@ -26,4 +26,14 @@ echo.
 echo ".Ok"
 
 echo.
+echo "   - PROXY_WINDOWS_PATH: " %PROXY_WINDOWS_PATH%
+echo.
+echo "   - http_proxy : " %http_proxy%
+echo "   - https_proxy: " %https_proxy%
+echo "   - all_proxy: " %all_proxy%
+echo.
+echo "   - no_proxy : " %no_proxy%
+echo.
+
+echo.
 PAUSE
